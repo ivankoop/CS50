a21a8484ff966b9b03f08e879fd4646d42559dbc
ivan koop <ivankoop3@gmail.com>
2019-07-19 22:52:51 -0400

    more implementations

--- project-1/App.js
+++ project-1/App.js
@@ -1,15 +1,79 @@
 import React from "react";
-import {StyleSheet, Text, View, Button} from "react-native";
+import {StyleSheet, Text, View, TouchableOpacity, Picker} from "react-native";
 import {vibrate} from "./utils";
 import moment from "moment";
-// causes phone to vibrate
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+        alignItems: "center",
+    },
+
+    timerText: {
+        fontSize: 30,
+        top: 100,
+    },
+
+    selectContainer: {
+        flex: 1,
+        alignItems: "center",
+    },
+
+    selectBtn: {
+        marginTop: 150,
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingTop: 5,
+        paddingBottom: 5,
+        borderRadius: 5,
+        backgroundColor: "#5844ed",
+    },
+
+    startBtn: {
+        backgroundColor: "#32CD32",
+        marginTop: 10,
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingTop: 5,
+        paddingBottom: 5,
+        borderRadius: 5,
+    },
+
+    picker: {
+        width: 300,
+        marginTop: 100,
+    },
+
+    actionsContainer: {
+        flex: 1,
+        alignItems: "center",
+        marginTop: 100,
+    },
+
+    pauseBtn: {
+        backgroundColor: "#ff1919",
+        marginTop: 80,
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingTop: 5,
+        paddingBottom: 5,
+        borderRadius: 5,
+        alignItems: "center",
+        width: 130,
+    },
+});
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             timer: "00:00:00",
-            running: true,
+
+            time: 0,
+            showPicker: false,
+            timerStarted: false,
+            timerPaused: true,
         };
 
         this.startTimer();
@@ -17,9 +81,16 @@ export default class App extends React.Component {
 
     startTimer = () => {
         const tick = 1000;
-        let duration = 10 * 1000;
+        const {time} = this.state;
+        let duration = time * 1000;
 
         const interval = setInterval(() => {
+            let {timerPaused} = this.state;
+
+            if (timerPaused) {
+                return;
+            }
+
             duration = moment.duration(duration - tick, "milliseconds");
 
             let hours = duration.hours();
@@ -46,25 +117,100 @@ export default class App extends React.Component {
         vibrate();
     };
 
+    _selectTime = () => {
+        this.setState({showPicker: true});
+    };
+
+    _startTimer = () => {
+        this.setState({timerStarted: true});
+    };
+
+    _pauseTimer = () => {
+        this.setState(prevState => ({
+            timerPaused: !prevState.timerPaused,
+        }));
+    };
+
+    _resetTimer = () => {
+        this.setState({
+            timer: "00:00:00",
+            running: false,
+            showPicker: false,
+            timerStarted: false,
+            timerPaused: false,
+        });
+    };
+
+    _valueChanged = value => {
+        this.setState({time: value});
+    };
+
     render() {
-        const {timer} = this.state;
+        const {timer, showPicker, timerStarted, timerPaused} = this.state;
         return (
             <View style={styles.container}>
                 <Text style={styles.timerText}>{timer}</Text>
+                {timerStarted ? (
+                    <View style={styles.actionsContainer}>
+                        <TouchableOpacity
+                            style={{
+                                ...styles.pauseBtn,
+                                ...{
+                                    backgroundColor: timerPaused
+                                        ? "#32CD32"
+                                        : "#ff1919",
+                                },
+                            }}
+                            onPress={this._pauseTimer}
+                        >
+                            <Text style={{color: "white", fontSize: 20}}>
+                                {timerPaused ? "Continue" : "Pause"}
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.startBtn}
+                            onPress={this._resetTimer}
+                        >
+                            <Text style={{color: "white", fontSize: 20}}>
+                                Reset Timer
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                ) : showPicker ? (
+                    <View style={styles.selectContainer}>
+                        <Picker
+                            selectedValue={this.state.language}
+                            style={styles.picker}
+                            onValueChange={(itemValue, _itemIndex) =>
+                                this.setState({time: itemValue})
+                            }
+                        >
+                            <Picker.Item label="25" value="25" />
+                            <Picker.Item label="20" value="20" />
+                            <Picker.Item label="15" value="15" />
+                            <Picker.Item label="10" value="10" />
+                            <Picker.Item label="5" value="5" />
+                        </Picker>
+                        <TouchableOpacity
+                            style={styles.startBtn}
+                            onPress={this._startTimer}
+                        >
+                            <Text style={{color: "white", fontSize: 20}}>
+                                Start Timer
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                ) : (
+                    <TouchableOpacity
+                        style={styles.selectBtn}
+                        onPress={this._selectTime}
+                    >
+                        <Text style={{color: "white", fontSize: 20}}>
+                            Select Time
+                        </Text>
+                    </TouchableOpacity>
+                )}
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        top: 100,
-    },
-
-    timerText: {
-        fontSize: 30,
-    },
-});
